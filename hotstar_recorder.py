7020d7fe13422e996cc10cb224a1c0e20f417f98
praneeth-coder-2005 <savediskofc@gmail.com>
2024-10-30 19:23:22 +0530

    Update hotstar_recorder.py

--- hotstar_recorder.py
+++ hotstar_recorder.py
@@ -1,23 +1,22 @@
-   from Flask import Flask
-   import os
-   import time
-   from selenium import webdriver
-   from selenium.webdriver.common.by import By
+from flask import flask
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-   app = Flask(__name__)
+app = Flask(__name__)
 
-   @app.route('/start_recording')
-   def start_recording():
-       # Your login and recording logic
-       print("Recording Started")  # Add real recording logic here
-       return "Recording Started"
+@app.route('/start_recording')
+def start_recording():
+    # Your login and recording logic
+    print("Recording Started")  # Add real recording logic here
+    return "Recording Started"
 
-   @app.route('/stop_recording')
-   def stop_recording():
-       # Code to stop the recording
-       print("Recording Stopped")  # Add real stopping logic here
-       return "Recording Stopped"
+@app.route('/stop_recording')
+def stop_recording():
+    # Code to stop the recording
+    print("Recording Stopped")  # Add real stopping logic here
+    return "Recording Stopped"
 
-   if __name__ == "__main__":
-       app.run(host='0.0.0.0', port=8080)
-   
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
